89e1d3db4d62e56eca34d93bea1b69469189038b
Juluru Akhil <akhil.juluru@gmail.com>
2021-03-18 20:44:19 +0530

    Added Google Login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import GoogleLogin from "react-google-login";
 
 function App() {
+  const responseGoogle = (res) => {
+    console.log(res);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId="1019792659480-v1lvsoqi495r5u4qclg70eothoorob5h.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+        scope="https://www.googleapis.com/auth/contacts.readonly email profile"
+      />
     </div>
   );
 }
